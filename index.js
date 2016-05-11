65d4dfa77f80d7fab6e8634c1f3261ea2a30d825
kunalbarve <knbarve@paypal.com>
2016-05-11 16:11:56 -0700

    small change in local

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var options = {
 
 app.use(kraken(options));
 app.use(express.static(__dirname + '/public/app'));
-app.use("/bower_components", express.static(path.join(__dirname, 'public/bower_components')));
+app.use("/bower_components", express.static(__dirname+ '/public/bower_components'));
 /*
 app.listen(port, function (err) {
     console.log('Application ready to serve requests.');
@@ -27,7 +27,7 @@ app.listen(port, function (err) {
 
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
- 
+
 app.listen(app.get('port'), app.get('ip'), function () {
   console.log( "Listening on " + app.get('ip') + ", server_port " + app.get('port') )
 });
